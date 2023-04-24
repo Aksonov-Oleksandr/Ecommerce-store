7c5e9fd224578d4a4b51a15b7abad27d7862333c
Oleksandr Aksonov <AksonovOleksandr@hotmail.com>
2023-04-24 19:06:18 +0300

    Created react app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+       <a href="/">Security Pro</a>
+      </header>
+      <main>
+        products list
+      </main>
+    </div>
+  );
+}
+
+export default App;

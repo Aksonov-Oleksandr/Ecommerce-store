c06dfa7bdf278c3faa1500be826c8e19b2f89f41
Oleksandr Aksonov <AksonovOleksandr@hotmail.com>
2023-05-01 20:34:04 +0300

    integrated db, described product schema, added product and seed routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
-import express from "express"
+import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(()=> {
+    console.log('connected to db');
+}).catch( err => {
+    console.log(err.message);
+})
 
 const app = express();
 

8657503af9cbab5c79de8a43312ce98ab5b5c1fc
Oleksandr Aksonov <AksonovOleksandr@hotmail.com>
2023-05-02 16:32:30 +0300

    jwt auth for api, token generator,user router

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -9,7 +9,6 @@ const seedRouter = express.Router();
 seedRouter.get('/', async (req, res) => {
     await Product.deleteMany({});
     const createdProducts = await Product.insertMany(data.products);
-
     await User.deleteMany({});
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdProducts, createdUsers });

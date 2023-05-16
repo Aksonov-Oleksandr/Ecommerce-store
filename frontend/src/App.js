66954c0ae59fe2c59410adb105e8a10afc4c11d8
Oleksandr Aksonov <AksonovOleksandr@hotmail.com>
2023-05-16 16:53:09 +0300

    User Profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,7 @@ import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
     const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -92,6 +93,7 @@ function App() {
                <Route path="/cart" element={<CartScreen/>}></Route>
                <Route path="/signin" element={<SigninScreen/>} />
                <Route path="/signup" element={<SignupScreen />} />
+               <Route path="/profile" element={<ProfileScreen />} />
                <Route path="/placeorder" element={<PlaceOrderScreen />} />
                <Route path="/order/:id" element={<OrderScreen />}></Route>
                   <Route
